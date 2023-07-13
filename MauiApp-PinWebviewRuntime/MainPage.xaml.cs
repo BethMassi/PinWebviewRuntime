23fb58aec617bd9731e398236c298b7507b5ce2e
Beth Massi <bethma@microsoft.com>
2023-07-12 17:09:12 -0700

    Add project files.

---
+++ MauiApp-PinWebviewRuntime/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp_PinWebviewRuntime;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiApp-PinWebviewRuntime/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiApp_PinWebviewRuntime.Data;
+
+namespace MauiApp_PinWebviewRuntime;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+#if WINDOWS
+        string relativePath = @"..\Runtime";
+        string basePath = AppContext.BaseDirectory;
+        string wvrPath = Path.Combine(basePath, relativePath);        
+        Environment.SetEnvironmentVariable("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", wvrPath);
+#endif
+
+        var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
